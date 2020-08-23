f258d80ccb81caaef44bfc85e2846a9d5416f056
tripteshSharma <tripteshSharma2@gmail.com>
2020-08-23 22:32:41 +0530

    -- Implemented three A's fixed price promotion calculations.

---
+++ PromotionEngine/PromotionEngine.Service/Contracts/IPromotionApplyService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine.Service.Contracts
+{
+    public interface IPromotionApplyService
+    {
+        public int PromotionApply(List<string> skuType);
+    }
+}
---
+++ PromotionEngine/PromotionEngine.Service/Services/ThreeAFixPricePromotion.cs
@@ -0,0 +1,33 @@
+﻿using PromotionEngine.Service.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine.Service.Services
+{
+    public class ThreeAFixPricePromotion : IPromotionApplyService
+    {
+        public int PromotionApply(List<string> skuType)
+        {
+            try
+            {
+                var remainder = skuType.Count % 3;
+                int price = 0;
+                for (int typeCount = 3; typeCount <= skuType.Count; typeCount += 3)
+                {
+                    price += 130;
+                }
+                if (remainder > 0)
+                {
+                    price += (remainder * 50);
+                }
+
+                return price;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
